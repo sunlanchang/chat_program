14780b5469bbc1472daea5906da6e88196ab9b82
sunlanchang <sunlanchang@outlook.com>
2018-04-14 22:49:16 +0800

    实现发送信息

--- client.cs
+++ client.cs
@@ -1,23 +1,30 @@
 using System;
-using System.Net; // 引入这两个命名空间，以下同
+using System.Net;
+using System.Text; // 引入这两个命名空间，以下同
 using System.Net.Sockets;
 namespace chat_program {
     class Client {
         static void Main (string[] args) {
-
             Console.WriteLine ("Client Running ...");
-            for (int i = 0; i < 3; i++) {
-                TcpClient client = new TcpClient ();
-                try {
-                    client.Connect ("localhost", 8500); // 与服务器连接
-                } catch (Exception ex) {
-                    Console.WriteLine (ex.Message);
-                    return;
-                }
-                // 打印连接到的服务端信息
-                Console.WriteLine ("Server Connected！{0} --> {1}",
-                    client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
+            TcpClient client;
+
+            try {
+                client = new TcpClient ();
+                client.Connect ("localhost", 8500); // 与服务器连接
+            } catch (Exception ex) {
+                Console.WriteLine (ex.Message);
+                return;
             }
+            // 打印连接到的服务端信息
+            Console.WriteLine ("Server Connected！{0} --> {1}",
+                client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
+
+            string msg = "\"Welcome To TraceFact.Net\"";
+            NetworkStream streamToServer = client.GetStream ();
+
+            byte[] buffer = Encoding.Unicode.GetBytes (msg); // 获得缓存
+            streamToServer.Write (buffer, 0, buffer.Length); // 发往服务器
+            Console.WriteLine ("Sent: {0}", msg);
             // 按Q退出
             Console.WriteLine ("\n\n输入\"Q\"键退出。");
             ConsoleKey key;
--- server.cs
+++ server.cs
@@ -1,23 +1,34 @@
 using System;
 using System.Net; // 引入这两个命名空间，以下同
 using System.Net.Sockets;
+using System.Text;
 namespace chat_program {
     class Server {
         static void Main (string[] args) {
+            const int BufferSize = 8192; // 缓存大小，8192字节
+
             Console.WriteLine ("Server is running ... ");
             IPAddress ip = new IPAddress (new byte[] { 127, 0, 0, 1 });
             TcpListener listener = new TcpListener (ip, 8500);
 
             listener.Start (); // 开始侦听
             Console.WriteLine ("Start Listening ...");
-            while (true) {
-                // 获取一个连接，同步方法
-                TcpClient remoteClient = listener.AcceptTcpClient ();
-                // 打印连接到的客户端信息
-                Console.WriteLine ("Client Connected！{0} <-- {1}",
-                    remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
-            }
 
+            // 获取一个连接，中断方法
+            TcpClient remoteClient = listener.AcceptTcpClient ();
+            // 打印连接到的客户端信息
+            Console.WriteLine ("Client Connected！{0} <-- {1}",
+                remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
+
+            // 获得流，并写入buffer中
+            NetworkStream streamToClient = remoteClient.GetStream ();
+            byte[] buffer = new byte[BufferSize];
+            int bytesRead = streamToClient.Read (buffer, 0, BufferSize);
+            Console.WriteLine ("Reading data, {0} bytes ...", bytesRead);
+
+            // 获得请求的字符串
+            string msg = Encoding.Unicode.GetString (buffer, 0, bytesRead);
+            Console.WriteLine ("Received: {0}", msg);
             Console.WriteLine ("\n\n输入\"Q\"键退出。");
             ConsoleKey key;
             do {
