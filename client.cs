afffb8fb9da79849da66331fcaf3a473220f21bb
sunlanchang <sunlanchang@outlook.com>
2018-04-15 10:34:48 +0800

    实现 客户端发送，服务器接受后发给客户端

--- client.cs
+++ client.cs
@@ -7,6 +7,8 @@ namespace chat_program {
         static void Main (string[] args) {
             Console.WriteLine ("Client Running ...");
             TcpClient client;
+            ConsoleKey key;
+            const int BufferSize = 8192;
 
             try {
                 client = new TcpClient ();
@@ -15,19 +17,48 @@ namespace chat_program {
                 Console.WriteLine (ex.Message);
                 return;
             }
+
             // 打印连接到的服务端信息
             Console.WriteLine ("Server Connected！{0} --> {1}",
                 client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
 
-            string msg = "\"Welcome To TraceFact.Net\"";
             NetworkStream streamToServer = client.GetStream ();
+            Console.WriteLine ("Menu: S - Send, X - Exit");
 
-            byte[] buffer = Encoding.Unicode.GetBytes (msg); // 获得缓存
-            streamToServer.Write (buffer, 0, buffer.Length); // 发往服务器
-            Console.WriteLine ("Sent: {0}", msg);
-            // 按Q退出
+            do {
+                key = Console.ReadKey (true).Key;
+
+                if (key == ConsoleKey.S) {
+                    // 获取输入的字符串
+                    Console.Write ("Input the message: ");
+                    string msg = Console.ReadLine ();
+
+                    byte[] buffer = Encoding.Unicode.GetBytes (msg); // 获得缓存
+                    try {
+                        lock (streamToServer) {
+                            streamToServer.Write (buffer, 0, buffer.Length); // 发往服务器
+                        }
+                        Console.WriteLine ("Sent: {0}", msg);
+
+                        int bytesRead;
+                        buffer = new byte[BufferSize];
+                        lock (streamToServer) {
+                            bytesRead = streamToServer.Read (buffer, 0, BufferSize);
+                        }
+                        msg = Encoding.Unicode.GetString (buffer, 0, bytesRead);
+                        Console.WriteLine ("Received: {0}", msg);
+
+                    } catch (Exception ex) {
+                        Console.WriteLine (ex.Message);
+                        break;
+                    }
+                }
+            } while (key != ConsoleKey.X);
+
+            streamToServer.Dispose ();
+            client.Close ();
+
             Console.WriteLine ("\n\n输入\"Q\"键退出。");
-            ConsoleKey key;
             do {
                 key = Console.ReadKey (true).Key;
             } while (key != ConsoleKey.Q);
--- server.cs
+++ server.cs
@@ -5,7 +5,8 @@ using System.Text;
 namespace chat_program {
     class Server {
         static void Main (string[] args) {
-            const int BufferSize = 8192; // 缓存大小，8192字节
+            const int BufferSize = 8192; // 缓存大小，8192Bytes
+            ConsoleKey key;
 
             Console.WriteLine ("Server is running ... ");
             IPAddress ip = new IPAddress (new byte[] { 127, 0, 0, 1 });
@@ -14,23 +15,48 @@ namespace chat_program {
             listener.Start (); // 开始侦听
             Console.WriteLine ("Start Listening ...");
 
-            // 获取一个连接，中断方法
+            // 获取一个连接，同步方法，在此处中断
             TcpClient remoteClient = listener.AcceptTcpClient ();
+
             // 打印连接到的客户端信息
             Console.WriteLine ("Client Connected！{0} <-- {1}",
                 remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
 
-            // 获得流，并写入buffer中
+            // 获得流
             NetworkStream streamToClient = remoteClient.GetStream ();
-            byte[] buffer = new byte[BufferSize];
-            int bytesRead = streamToClient.Read (buffer, 0, BufferSize);
-            Console.WriteLine ("Reading data, {0} bytes ...", bytesRead);
 
-            // 获得请求的字符串
-            string msg = Encoding.Unicode.GetString (buffer, 0, bytesRead);
-            Console.WriteLine ("Received: {0}", msg);
+            do {
+                // 写入buffer中
+                byte[] buffer = new byte[BufferSize];
+                int bytesRead;
+                try {
+                    lock (streamToClient) {
+                        bytesRead = streamToClient.Read (buffer, 0, BufferSize);
+                    }
+                    if (bytesRead == 0) throw new Exception ("读取到0字节");
+                    Console.WriteLine ("Reading data, {0} bytes ...", bytesRead);
+
+                    // 获得请求的字符串
+                    string msg = Encoding.Unicode.GetString (buffer, 0, bytesRead);
+                    Console.WriteLine ("Received: {0}", msg);
+
+                    // 转换成大写并发送
+                    msg = msg.ToUpper ();
+                    buffer = Encoding.Unicode.GetBytes (msg);
+                    lock (streamToClient) {
+                        streamToClient.Write (buffer, 0, buffer.Length);
+                    }
+                    Console.WriteLine ("Sent: {0}", msg);
+                } catch (Exception ex) {
+                    Console.WriteLine (ex.Message);
+                    break;
+                }
+            } while (true);
+
+            streamToClient.Dispose ();
+            remoteClient.Close ();
+
             Console.WriteLine ("\n\n输入\"Q\"键退出。");
-            ConsoleKey key;
             do {
                 key = Console.ReadKey (true).Key;
             } while (key != ConsoleKey.Q);
